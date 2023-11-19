82d89e38ffd59101c492da078fdabc5e642672a2
luisagd <luisaguero011@gmail.com>
2023-11-18 23:56:28 -0300

    checking if ghpages is working using only branches

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,7 +134,7 @@ const IndexPage = () => {
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. ;jhbnfskdljbfalksjdbflakjsdb😎
+        update in real-time. ;jhbnfskdljbfalksjdbflakjsdbjjjjjjjjjjjjjjjjjj😎
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
